72a89c5e963d2783f2e167fef12f8c99312d6102
EliabParra <eliabparra@gmail.com>
2026-01-09 10:58:03 -0400

    add core components and pages with initial templates and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/pages/home/home.component';
+import { ProductsComponent } from './core/pages/products/products.component';
+import { StoresComponent } from './core/pages/stores/stores.component';
+import { StockComponent } from './core/pages/stock/stock.component';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    StoresComponent,
+    StockComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/interfaces/Inventory.ts
@@ -0,0 +1,6 @@
+export interface Inventory {
+  id: number
+  productId: number
+  storeId: number
+  quantity: number
+}
